5ccf5576762a28b9627fe639a04620ac0cf9ed5d
Elizabeth <pascual12eli2002@gmail.com>
2026-01-27 22:19:03 -0600

    Primer commit

---
+++ clase_1/tipo_datos.js
@@ -0,0 +1,37 @@
+// Ejemplo_base
+console.log(typeof 42); //number
+
+// Valores solicitados
+console.log(typeof 42);              // number
+console.log(typeof 'Veinticinco');   // string
+console.log(typeof -666);            // number
+console.log(typeof true);            // boolean
+console.log(typeof 0);               // number
+console.log(typeof "");              // string (cadena vacía)
+console.log(typeof null);            // object (caso especial en JS)
+console.log(typeof undefined);       // undefined
+console.log(typeof false);           // boolean
+
+// ----------------------------
+// String
+console.log(typeof "Hola soy un string");   // string
+console.log(typeof 'yo tambien soy un string'); // string
+
+// Numbers
+console.log(typeof 1.2);   // number
+console.log(typeof 24);    // number
+console.log(typeof 45);    // number
+console.log(typeof -1.1);  // number
+
+// Booleanos
+console.log(typeof true);   // boolean
+console.log(typeof false);  // boolean
+
+// Null
+console.log(typeof null);   // object (error histórico de JS)
+
+// Undefined
+console.log(typeof undefined); // undefined
+
+/* este es un comentario para
+varias lineas de texto */
